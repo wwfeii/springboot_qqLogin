9f64c030523ed424cd99367e8fb71ba3bf6fbdcc
wwfeii <748999979@qq.com>
2018-04-17 21:24:09 +0800

    qq第三方登陆

---
+++ springboot-qqlogin/src/main/java/com/tfyh/QqloginApplication.java
@@ -0,0 +1,12 @@
+package com.tfyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqloginApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(QqloginApplication.class,args);
+	}
+
+}
---
+++ springboot-qqlogin/src/main/java/com/tfyh/controller/QqloginController.java
@@ -0,0 +1,69 @@
+package com.tfyh.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.qq.connect.QQConnectException;
+import com.qq.connect.api.OpenID;
+import com.qq.connect.api.qzone.UserInfo;
+import com.qq.connect.javabeans.AccessToken;
+import com.qq.connect.javabeans.qzone.UserInfoBean;
+import com.qq.connect.oauth.Oauth;
+/**
+ * 
+ * @author wangfei
+ * qq第三方登录
+ */
+@Controller
+public class QqloginController {
+	/**
+	 * 重定向到qq授权页面
+	 * @param request
+	 * @return
+	 * @throws QQConnectException 
+	 */
+	@RequestMapping(value="/authorizeUrl")
+	public String authorizeUrl(HttpServletRequest request) throws QQConnectException{
+		String authorizeUrl = new Oauth().getAuthorizeURL(request);
+		return "redirect:"+authorizeUrl;
+	}
+	
+	/**
+	 * qq授权回调路径
+	 * @param request
+	 * @return
+	 * @throws QQConnectException 
+	 */
+	@RequestMapping(value="/qqLoginCallback",method=RequestMethod.GET)
+	@ResponseBody
+	public String qqLoginCallback(HttpServletRequest request) throws QQConnectException{
+		//通过回调中的code得到accessToken
+		AccessToken accessTokenObj = new Oauth().getAccessTokenByRequest(request);
+		String accessToken = accessTokenObj.getAccessToken();
+		if(accessToken == null || "".equals(accessToken)){
+			throw new QQConnectException("accessToken为空，授权失败");
+		}
+		//通过accessToken得到openId
+		OpenID openIdObj = new OpenID(accessToken);
+		if(openIdObj == null || "".equals(openIdObj.getUserOpenID())){
+			throw new QQConnectException("openIdObj为空，授权失败");
+		}
+		String openId = openIdObj.getUserOpenID();
+		//通过accessToken和openId得到用户信息
+		UserInfo qzoneUserInfo = new UserInfo(accessToken, openId);
+		UserInfoBean userInfoBean = qzoneUserInfo.getUserInfo();
+		if(userInfoBean == null || userInfoBean.getRet() != 0){
+			throw new QQConnectException(userInfoBean.getMsg()+",授权失败");
+		}
+		//得到用户昵称
+		String nickname = userInfoBean.getNickname();
+		String imgUrl = userInfoBean.getAvatar().getAvatarURL30();
+		return "nickName="+nickname+"   imgUrl="+imgUrl;
+	}
+	
+
+}
